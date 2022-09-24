d4de69e879375bd76d9af036ea1d7af2be518fac
danielfbbaptista94 <daniel94.baptista@gmail.com>
2022-09-24 14:05:23 -0300

    Fix login without email verification

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:mynotes/constants/routes.dart';
-import 'dart:developer' as devtools show log;
 
 import '../enums/menu_actions.dart';
 
